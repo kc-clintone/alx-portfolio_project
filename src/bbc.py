3400c461dbec9da8f156bf3eb7d92019640dfcb2
kc-clintone <kayseeclintone@gmail.com>
2024-10-17 13:03:17 +0300

    Working on moves generator

--- src/bbc.py
+++ src/bbc.py
@@ -3,7 +3,7 @@
 This is the main module, responsible for running the game
 """
 import json
-
+from moves import move_gen
 
 # the main class (loading game config)
 class Game:
@@ -20,3 +20,10 @@ class Game:
                 self.fen_string.split()[0].replace('/', '\n ')
             ]) + '\n' + '        \n' * 2)
             self.turn = 0 if self.fen_string.split()[1] == 'w' else 1
+
+# Game config/settings
+GAME_SETTINGS = '../src/config.json'
+
+bbc = Game(GAME_SETTINGS)
+bbc.move_gen()
+# print(''.join([' ' + bbc.pieces[p] for p in ''.join(bbc.board)]), (bbc.turn))
